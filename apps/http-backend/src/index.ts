442aa18b52df8a1aea320cddf383edd23504afc0
mohdrafey1 <mohdrafey600@gmail.com>
2025-01-26 13:15:14 +0530

    initialize and created a db package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,17 +1,33 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleware';
+import { CreateUserSchema, SigninSchema } from '@repo/common/types';
 
 const app = express();
 
 app.post('/signup', (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: 'Incorrect Inputs',
+        });
+        return;
+    }
     res.json({
         userId: '123',
     });
 });
 
 app.post('/signin', (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: 'Incorrect Inputs',
+        });
+        return;
+    }
+
     const userId = 1;
     const token = jwt.sign(
         {
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Response, Request } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend-common/config';
 
 interface DecodedToken extends JwtPayload {
     userId: string;
