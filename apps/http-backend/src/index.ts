af5e5f659b2c8bb503a3dea759d862dc12b1249d
mohdrafey1 <mohdrafey600@gmail.com>
2025-01-29 21:11:02 +0530

    changes in signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,22 +3,37 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleware';
 import { CreateUserSchema, SigninSchema } from '@repo/common/types';
+import { prismaClient } from '@repo/db/client';
 
 const app = express();
 
-app.post('/signup', (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success) {
+app.post('/signup', async (req, res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        console.log(parsedData.error);
         res.json({
-            message: 'Incorrect Inputs',
+            message: 'Incorrect inputs',
         });
         return;
     }
-    res.json({
-        userId: '123',
-    });
+    // i will hash this pass later
+    try {
+        const user = await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name,
+            },
+        });
+        res.json({
+            userId: user.id,
+        });
+    } catch (e) {
+        res.status(411).json({
+            message: 'User already exists with this username',
+        });
+    }
 });
-
 app.post('/signin', (req, res) => {
     const data = SigninSchema.safeParse(req.body);
     if (!data.success) {
--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,1 +1,1 @@
-export const JWT_SECRET = 'rafey';
+export const JWT_SECRET = process.env.JWT_SECRET || '123123';
